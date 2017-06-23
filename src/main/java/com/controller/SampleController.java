dfd18d005a852584dd2ef211dd5baca1af4ad7c1
Suresh VPS <suresh.v06@infosys.com>
2017-06-23 14:01:10 +0530

    mongo DB added

--- src/main/java/com/controller/SampleController.java
+++ src/main/java/com/controller/SampleController.java
@@ -1,19 +1,25 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 @EnableAutoConfiguration
 @ComponentScan
 @EnableCaching
-public class SampleController {
+public class SampleController extends SpringBootServletInitializer {
 	
-
+	@Autowired
+	CustomerRepository repo;
+	
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -23,8 +29,16 @@ public class SampleController {
     @RequestMapping("/create")
     @ResponseBody
     String save(String firstName, String lastName) {
-    	
-        return "back end code is not yet integrated";
+    	Customer cus = new Customer(firstName,lastName);
+    	repo.save(cus);
+        return "check your DB";
+    }
+    
+    @RequestMapping("/find")
+    @ResponseBody
+    String find(String firstName) {
+    	Customer cus = repo.findByFirstName(firstName);
+        return cus.lastName;
     }
     
 
@@ -32,4 +46,11 @@ public class SampleController {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
     }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<SampleController> applicationClass = SampleController.class;
 }
