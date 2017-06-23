c8d5fb35eb1c7e229fab36ff78fca3d2fcb5528d
Suresh VPS <suresh.v06@infosys.com>
2017-06-23 14:23:58 +0530

    repository classes added

---
+++ src/main/java/com/controller/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+    //public List<Customer> findByLastName(String lastName);
+
+}
